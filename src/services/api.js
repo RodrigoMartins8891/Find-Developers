b556f5305ffa366058e51e36e688ad72cd280dd0
Rodrigo Martins de Oliveira <seuemaildopgithub@gmail.com>
2026-03-31 19:12:16 -0300

    test env token

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const token = import.meta.env.VITE_GITHUB_TOKEN;
 
+console.log("TOKEN:", token);
+
 export const api = axios.create({
   baseURL: "https://api.github.com",
   headers: {
