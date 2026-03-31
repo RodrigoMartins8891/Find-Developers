9f541389971d549cf923ceaae4c79dafef3abe4a
Rodrigo Martins de Oliveira <seuemaildopgithub@gmail.com>
2026-03-31 19:26:28 -0300

    fix finaly and deploy

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,10 @@ import axios from "axios";
 
 const token = import.meta.env.VITE_GITHUB_TOKEN;
 
-console.log("TOKEN:", token);
-
 export const api = axios.create({
   baseURL: "https://api.github.com",
   headers: {
     Accept: "application/vnd.github+json",
-    ...(token && { Authorization: `token ${token}` }),
+    ...(token ? { Authorization: `token ${token}` } : {}),
   },
 });
